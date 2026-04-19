2b7cfc031299ffe2d17cf20bad22f3c47a24053c
lyi <157193736+shrlijltii@users.noreply.github.com>
2026-04-19 11:36:09 +0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+a = 2
+b = 3
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+def multiply(x, y):
+    return x * y
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Test fitur2"
+def power(x, y):
+    return x ** y
+def modulus(x, y):
+    return x % y
